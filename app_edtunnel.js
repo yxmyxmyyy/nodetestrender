ba96f472316f28505777e4cb9f9a9f1d86be0f0c
YANG XIMING <yxm1010122195@gmail.com>
2023-11-25 17:20:44 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'fe79d20d-b1b7-47cf-a3fc-545c94df5733';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
